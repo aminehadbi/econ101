5c2a72e995e1d930ba05fff3b1d865002ea03b24
Paul Schrimpf <paul.schrimpf@gmail.com>
2013-09-04 13:25:44 -0700

    Minor changes.

--- syncWeb.sh
+++ syncWeb.sh
@@ -13,5 +13,6 @@ mkdir -p /tmp/rsync
 find . -regex ".*lec[0-9][0-9].*pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
 #find problemSets -iname "526*.pdf" -not -iname "*sol*pdf" -exec rsync -tvhc  --progress  --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
 find . -iname "*.html" -exec rsync -tvhc --progress --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
+find . -iname "526-syllabus.pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
 
 
