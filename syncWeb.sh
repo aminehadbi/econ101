7749803dbe6586b7342450ecb6661f88393c99c9
Paul Schrimpf <paul.schrimpf@gmail.com>
2013-09-03 18:10:59 -0700

    new file:   syncWeb.sh

---
+++ syncWeb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# mount arts server if needed
+if grep -qs '/mnt/arts' /proc/mounts; then
+    echo ""
+else 
+    echo "Mounting arts ftp server"
+    exec "/home/paul/mountArts.sh"
+fi
+
+mkdir -p /tmp/rsync
+
+find . -regex ".*lec[0-9][0-9].*pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
+#find problemSets -iname "526*.pdf" -not -iname "*sol*pdf" -exec rsync -tvhc  --progress  --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
+find . -iname "*.html" -exec rsync -tvhc --progress --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
+
+
