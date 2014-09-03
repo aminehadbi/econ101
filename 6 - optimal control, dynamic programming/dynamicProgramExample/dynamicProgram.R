66ebc1e5dacb1587a72f9de13b224eb3ba43eb19
Paul Schrimpf <paul.schrimpf@gmail.com>
2014-09-03 16:35:16 -0700

    Began updating notes for 2014.

--- 6 - optimal control, dynamic programming/dynamicProgramExample/dynamicProgram.R
+++ 6 - optimal control, dynamic programming/dynamicProgramExample/dynamicProgram.R
@@ -1,8 +1,10 @@
 rm(list=ls())
 
 # Parameter settings
-discountFactor <- 0.8
-returnsToScale <- 0.6
+discountFactor <- 0.9
+returnsToScale <- 1.0
+TFP <- 1.0
+depreciation <- 1.0
 
 ## Settings for value function approximation
 ## We approximate V(k) by linearly interpolating between on k.grid
@@ -13,41 +15,52 @@ iterations <- 50 ## number of bellmanOperator iterations
 productionFunction <- function(k) {
     return(k^returnsToScale)
 }
-utilityFunction <- function(c) {
-    return(log(c))
+utilityFunction <- function(c,k) {
+    return(TFP*k - c - c^2/(2*k))
+}
+transitionFunction <- function(c,k) {
+    return(c + (1-depreciation)*productionFunction(k))
 }
 
 ## bellman operator
 bellmanOperator <- function(V) {
     v.grid <- NA*k.grid
+    p.grid <- v.grid
     ## Compute value for each point in grid
     for (i in 1:length(k.grid)) {
         k.old <- k.grid[i]
-        obj <- function(k.new) {
-            utilityFunction(productionFunction(k.old) - k.new) + discountFactor*V(k.new)
+        obj <- function(c) {
+            utilityFunction(c,k.old) + discountFactor*V(transitionFunction(c,k.old))
         }
         ## maximize utility plus next value
-        res <- optimize(f=obj, interval=c(min(k.grid),
-                                   productionFunction(k.old)-0.001),
+        res <- optimize(f=obj, interval=c(-100,100),
                         maximum=TRUE)
         v.grid[i] <- res$objective
+        p.grid[i] <- res$maximum
     }
-    return(approxfun(x=k.grid, y=v.grid,rule=2))
+    return(list(value=approxfun(x=k.grid, y=v.grid,rule=2),
+                policy=approxfun(x=k.grid, y=p.grid,rule=2)))
 }
 
-v0 <- function(k) { return(0) }
+v0 <- function(k) { return(TFP*k) }
 v <- list()
 v.old <- v0
-df <- data.frame(k=k.grid,v=v0(k.grid),iteration=0)
+df <- data.frame(k=k.grid,v=v0(k.grid),c=0,iteration=0)
 for (t in 1:iterations) {
     v[[t]] <- bellmanOperator(v.old)
-    v.old <- v[[t]]
+    v.old <- v[[t]]$value
     cat("Completed ",t," of ",iterations," iterations.\n")
     if (t %% (floor(iterations/10))==0)
-    df <- rbind(df,data.frame(k=k.grid,v=v[[t]](k.grid),iteration=t))
+    df <- rbind(df,data.frame(k=k.grid,v=v[[t]]$value(k.grid),c=v[[t]]$policy(k.grid),iteration=t))
 }
 
 ## plot results
 library(ggplot2)
-ggplot(df,aes(x=k,y=v,colour=iteration,group=iteration)) + geom_line()
+c <- (1+discountFactor*depreciation) +
+      sqrt((1+discountFactor*depreciation)^2 -
+           discountFactor^2*(4*TFP)) / discountFactor^2
+ggplot(df,aes(x=k,y=v,colour=iteration,group=iteration)) +
+    geom_line() 
+
+
 
--- syncWeb.sh
+++ syncWeb.sh
@@ -11,6 +11,7 @@ fi
 mkdir -p /tmp/rsync
 
 find . -regex ".*lec[0-9][0-9].*pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
+find . -regex ".*-526.*pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
 #find problemSets -iname "526*.pdf" -not -iname "*sol*pdf" -exec rsync -tvhc  --progress  --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
 find . -iname "*.html" -exec rsync -tvhc --progress --temp-dir=/tmp/rsync {} /mnt/arts/526 \;
 find . -iname "526-syllabus.pdf" -exec rsync -tvhc  --progress --temp-dir=/tmp/rsync  {} /mnt/arts/526 \;
