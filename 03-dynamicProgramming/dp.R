1a5d01dfe5e8aecea57ce550d3a0f311407e10f8
Paul Schrimpf <paul.schrimpf@gmail.com>
2015-10-19 13:04:51 -0700

    Corrected typos in metric space notes.

--- 03-dynamicProgramming/dp.R
+++ 03-dynamicProgramming/dp.R
@@ -13,7 +13,7 @@ f <- expression(k^alpha)
 utility <- deriv(u, "c", function(c) {})
 # production function
 production <- deriv(expression(k^alpha), "k", function(k) {})
-# transition 
+# transition
 transition <- deriv(substitute(F + k*(1-depreciation) - c,
                                list(F=f[[1]])), c("k","c"),
                     function(c,k) {})
@@ -30,39 +30,57 @@ k.grid <- seq(0.01,3*kss$root,length.out=200)
 # quickly we converge
 #v.grid <- rep(0,length(k.grid))
 v.grid <- rep(uss/(1-discount),length(k.grid))
-#v.grid <- utility(k.grid*0.5)/(1-discount) 
-
+#v.grid <- utility(k.grid*0.5)/(1-discount)
+approx.method <- "spline"
+degree <- 10
+approxfn <- function(x,y) {
+  if (approx.method=="linear") {
+    return(approxfun(x, y,rule=2, method="linear"))
+  } else if (approx.method=="spline") {
+    return(splinefun(x, y, method="fmm"))
+  }
+  # least squares
+  else if (approx.method=="least-squares") {
+    model <- lm(y ~ poly(x, degree=degree))
+    f <- function(xnew) {
+      predict(model, newdata=data.frame(x=xnew))
+    }
+    return(f)
+  } else {
+    stop("unrecognized approx.method")
+  }
+}
 ## initial guess at value function
-v0 <- approxfun(k.grid, v.grid,rule=2, method="linear")
+v0 <- approxfn(k.grid, v.grid)
 library(parallel)
 v.change <- 100
 tol <- 1e-4
 iterations <- 0
 v.app <- list()
 v.app[[1]] <- v0
-while(v.change > tol || iterations<100) {
-    bellman <- mcmapply(function(k) {
-        optimize(function(c) {
-            utility(c) + discount*v0(transition(c,k)) },
-                 interval=c(0.01, production(k) + (1-depreciation)*k -
-                     0.01),
-                 maximum=TRUE,
-                 tol=tol*1e-3
-                 )
-    }, k.grid)
-    
-    v.g.new <- unlist(bellman["objective",])
-    
-    v.change <- max(abs(v.g.new-v.grid))
-    iterations <- iterations+1
-    v.grid <- v.g.new
-    v0 <- approxfun(k.grid,v.grid,rule=2,method="linear")
-    v.app[[iterations+1]] <- v0
-    print(sprintf("After %d iterations v.change=%.2g\n",iterations,v.change))
+while(v.change > tol && iterations<100) {
+  bellman <- mcmapply(function(k) {
+    optimize(function(c) {
+      utility(c) + discount*v0(transition(c,k)) },
+             interval=c(0.01, production(k) + (1-depreciation)*k -
+                 0.01),
+             maximum=TRUE,
+             tol=tol*1e-3
+             )
+  }, k.grid)
+
+  v.g.new <- unlist(bellman["objective",])
+
+  v.change <- max(abs(v.g.new-v.grid))
+  iterations <- iterations+1
+  v.grid <- v.g.new
+  v0 <- approxfn(k.grid,v.grid)
+  v.app[[iterations+1]] <- v0
+  print(sprintf("After %d iterations v.change=%.2g\n",iterations,v.change))
 }
 
 df <- data.frame(k=k.grid, v1=v.app[[1]](k.grid), v2=v.app[[2]](k.grid),
-                 v10=v.app[[10]](k.grid), v100=v.app[[100]](k.grid),
+                 v10=v.app[[10]](k.grid), v100=v.app[[min(100,length(v.app))]](k.grid),
                  v=v0(k.grid),
                  c=unlist(bellman["maximum",]),
                  k.new=transition(unlist(bellman["maximum",]),k.grid))
@@ -80,16 +98,16 @@ value <- ggplot(df,aes(x=k)) +
 policy <- ggplot(df,aes(x=k))+
     geom_line(aes(y=c,colour="consumption"))  +
     geom_line(aes(y=k.new,colour="next capital")) +
-    geom_line(aes(y=k),colour="gray") + 
+    geom_line(aes(y=k),colour="gray") +
     theme_minimal() +
     scale_colour_discrete(name="") +
     scale_y_continuous("consumption or capital") +
     scale_x_continuous("current capital")
 
-pdf("value.pdf",width=7,height=5)
+#pdf("value.pdf",width=7,height=5)
 value
-dev.off()
+#dev.off()
 
-pdf("policy.pdf",width=7,height=5)
+#pdf("policy.pdf",width=7,height=5)
 policy
-dev.off()
+#dev.off()
