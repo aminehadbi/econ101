1897b797431966df9f644f9ff3d0cb08d28715dc
Paul Schrimpf <paul.schrimpf@gmail.com>
2019-09-11 11:39:24 -0700

    Corrected location in syllabus. Added notes on numeric optimization.

---
+++ 01-optimization/julia/make.jl
@@ -0,0 +1,9 @@
+using Weave
+import Base64:stringmime
+weave("optimization.jmd",out_path=:doc,fig_path="figures",
+      mod=Main,
+      doctype="pandoc2html",
+      pandoc_options=["--toc","--toc-depth=2","--filter=pandoc-citeproc"],
+      cache=:refresh) 
+
+notebook("empiricalLikelihood.jmd")
